f32e60864c6eef65cbaa129b91aaf2c6cc69a4c7
Ling Lu <ling.lu916@gmail.com>
2019-12-18 19:21:20 -0500

    title box jankyness

--- game.js
+++ game.js
@@ -14,8 +14,12 @@ var myGameArea = {
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
+
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        // document.body.childNodes[0].appendChild(this.canvas) 
+        document.getElementById('gameCanvas').appendChild(this.canvas)
+        // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
             myGameArea.keys = (myGameArea.keys || []);

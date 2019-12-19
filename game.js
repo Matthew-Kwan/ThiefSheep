06a19f5e691c41e37cad04904ede87cfc6cc378a
Michael Jia <michael.zc.jia@gmail.com>
2019-12-18 19:53:18 -0500

    added empty start screen

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ var myGamePiece;
 var myGameB;
 
 function startGame() {
-    myGameArea.start();
+    myGameArea.prestart();
     myGamePiece = new component(30, 30, "red", 10, 120);
     myGameB = new component(30, 30, "blue", 11, 120);
 
@@ -11,21 +11,31 @@ function startGame() {
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
-    start : function() {
+    prestart : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
+        this.startIntervalId = setInterval(startGameArea, 20);
         window.addEventListener('keydown', function (e) {
             myGameArea.keys = (myGameArea.keys || []);
             myGameArea.keys[e.keyCode] = (e.type == "keydown");
         })
         window.addEventListener('keyup', function (e) {
-            myGameArea.keys[e.keyCode] = (e.type == "keydown");            
+            myGameArea.keys[e.keyCode] = (e.type == "keydown");
         })
+    },
+    start : function() {
+        myGameArea.mainIntervalId = setInterval(updateGameArea, 20);
+        // window.addEventListener('keydown', function (e) {
+        //     myGameArea.keys = (myGameArea.keys || []);
+        //     myGameArea.keys[e.keyCode] = (e.type == "keydown");
+        // })
+        // window.addEventListener('keyup', function (e) {
+        //     myGameArea.keys[e.keyCode] = (e.type == "keydown");
+        // })
 
-    }, 
+    },
     clear : function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
@@ -36,9 +46,9 @@ function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.speedX = 0;
-    this.speedY = 0;    
+    this.speedY = 0;
     this.x = x;
-    this.y = y;    
+    this.y = y;
     this.color = color;
     this.update = function() {
         ctx = myGameArea.context;
@@ -47,8 +57,18 @@ function component(width, height, color, x, y) {
     }
     this.newPos = function() {
         this.x += this.speedX;
-        this.y += this.speedY;        
-    }    
+        this.y += this.speedY;
+    }
+}
+
+function startGameArea() {
+
+    myGameArea.clear();
+
+    if (myGameArea.keys && myGameArea.keys[13]) {
+      clearInterval(myGameArea.startIntervalId);
+      myGameArea.start();
+    }
 }
 
 function updateGameArea() {
@@ -60,7 +80,7 @@ function updateGameArea() {
     myGameB.speedX =0;
     myGameB.speedY= 0;
 
-    if (myGameArea.keys && myGameArea.keys[32]) 
+    if (myGameArea.keys && myGameArea.keys[32])
         {myGamePiece.color = 'blue';
         myGamePiece.update()}
     if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
@@ -68,7 +88,7 @@ function updateGameArea() {
     if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -1; }
     if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 1; }
 
-    if (myGameArea.keys && myGameArea.keys[82]) 
+    if (myGameArea.keys && myGameArea.keys[82])
         {myGameB.color = 'red';
         myGameB.update()}
     if (myGameArea.keys && myGameArea.keys[65]) {myGameB.speedX = -1; }
@@ -77,18 +97,18 @@ function updateGameArea() {
     if (myGameArea.keys && myGameArea.keys[83]) {myGameB.speedY = 1; }
 
 
-    // if (myGameB.keys && myGameB.keys[32]) 
+    // if (myGameB.keys && myGameB.keys[32])
     //     {myGameB.color = 'blue';
     //     myGameB.update();}
 
-   
-    
-    myGameB.newPos(); 
+
+
+    myGameB.newPos();
     myGameB.update();
 
-    myGamePiece.newPos();    
+    myGamePiece.newPos();
 
     myGamePiece.update();
 
-    
+
 }
